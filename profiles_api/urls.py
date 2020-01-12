770d754370f9c6fdbf42f0686d041895f7b86f45
Ala <alajlif@live.fr>
2020-01-12 21:18:02 +0100

    Add viewset commit

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
 
 from profiles_api import views
 
+# viewset use router in order to generate the different routes available in ViewSet
+router = DefaultRouter()
+# no need to specify the / in the router endpoint
+router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
+
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('', include(router.urls))
 ]
